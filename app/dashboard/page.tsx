d23c9d21569ef39c7dfe1e7939df1e4b051e3671
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-20 00:14:53 +0300

    added qr code system

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,43 @@
-const Dashboard = () => {
+"use client";
+import QRCode from "react-qr-code";
+import { FormEvent, useState } from "react";
+
+export default function Page() {
+  const [submittedData, setSubmittedData] = useState<{ name: string } | null>(
+    null
+  );
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const name = formData.get("name")?.toString();
+
+    if (!name) {
+      console.error("Name is required");
+      return;
+    }
+
+    console.log({ name });
+    setSubmittedData({ name });
+  }
+
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <form>
-        <input
-          name="link"
-          type="text"
-          placeholder="Linkinizi girin"
-          className="mb-4 p-2 border border-gray-300 rounded"
-        />
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name" required />
+        <button type="submit">Submit</button>
       </form>
+      {submittedData && (
+        <div>
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "360px", width: "360px" }}
+            value={submittedData.name}
+            viewBox={`0 0 256 256`}
+          />
+        </div>
+      )}
     </div>
   );
-};
-
-export default Dashboard;
+}
