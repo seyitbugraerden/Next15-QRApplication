96c230f1b6d1d75afa179edab67d807598b3d325
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-25 00:48:30 +0300

    Home Page update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,12 @@
+import Hero from "@/components/hero";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="py-24">
+      <Hero />
+    </div>
+  );
 };
 
 export default HomePage;
